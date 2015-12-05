9f796b64a19bbda7962121fb14a1c8e00765ca59
Rui Miguel Silva Seabra <rms@1407.org>
2015-12-05 22:50:56 +0000

    start setting up the ldap parts

--- src/pam_ipahbac.c
+++ src/pam_ipahbac.c
@@ -24,14 +24,25 @@
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
+#ifdef HAVE_LDAP_H
+# include <ldap.h>
+#endif
+
 #include "pam_ipahbac.h"
 
 #define LEN 128
 
-int ipa_check_hbac(const char *thishost, const char *username) {
+int ipa_check_hbac(const char* ldapservers, const char* binduser, const char* bindpw, const char* thishost, const char* username) {
 	int matchuser=0;
 	int matchhost=0;
 	int retval=0;
+
+#ifdef HAVE_LDAP_H
+	LDAP* ld;
+	int result;
+	int auth_method=LDAP_AUTH_SIMPLE;
+	int version=LDAP_VERSION3;
+#endif
 	// int matchsvc=0; FIXE
 
 	if(strncmp("roque.1407.org", thishost, LEN) == 0) {
@@ -45,7 +56,7 @@ int ipa_check_hbac(const char *thishost, const char *username) {
 	return (matchuser && matchhost);
 }
 
-/* expected hook */
+/* credentials */
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
 	return PAM_IGNORE;
 }
@@ -58,38 +69,64 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	int retval;
 	int opt;
 	char thishost[LEN];
-	char binduser[LEN];
+	char binduser[LEN], sysaccount[LEN];
 	char bindpw[LEN];
 	char base[LEN];
+	char ldapservers[LEN];
 	const char* username=NULL;
+	int gotuser=0,gotpass=0,gotbase=0,gotservers=0;
 
 	retval = pam_get_user(pamh, &username, "Username: ");
 	if (retval != PAM_SUCCESS) {
 		return retval;
 	}
 
-	while( (opt = getopt(argc, (char * const*)argv, "u:p:b:") ) != -1 ) {
+	optind=0;
+	while( (opt = getopt(argc, (char * const*)argv, "u:p:b:l:") ) != -1 ) {
 		switch(opt) {
 			case 'u':
 				binduser[LEN-1]='\0';
-				strncpy(binduser, optarg, LEN-1); break;
+				strncpy(binduser, optarg, LEN-1);
+				gotuser=1;
+				break;
 			case 'p':
 				bindpw[LEN-1]='\0';
-				strncpy(bindpw, optarg, LEN-1); break;
+				strncpy(bindpw, optarg, LEN-1);
+				gotpass=1;
+				break;
 			case 'b':
 				base[LEN-1]='\0';
-				strncpy(base, optarg, LEN-1); break;
+				strncpy(base, optarg, LEN-1);
+				gotbase=1;
+				break;
+			case 'l':
+				ldapservers[LEN-1]='\0';
+				strncpy(ldapservers, optarg, LEN-1);
+				gotservers=1;
+				break;
 		}
+	}
+
+	if( ! (gotuser && gotpass && gotbase && gotservers ) ) {
+		printf("ERROR: missing -u, -p, -b or -l parameters (%d,%d,%d,%d). Please RTFM.\n", gotuser, gotpass, gotbase, gotservers);
+		return(PAM_PERM_DENIED);
+	}
+
+	retval = snprintf(sysaccount, LEN-1, "cn=%s,cn=sysaccounts,cn=etc,%s", binduser, base);
+	if( retval <= 0 ) {
+		printf("ERROR: failure defining the sysaccount for %s in %s\n", binduser, base);
+		return(PAM_PERM_DENIED);
 	}
 
 	thishost[LEN-1]='\0';
 	gethostname(thishost, LEN-1);
 	//printf("Hostname: %s\n", thishost);
-	//printf("Binduser: %s\n", binduser);
+	//printf("Binduser: %s\n", sysaccount);
 	//printf("Bindpw: %s\n", bindpw);
 	//printf("Base: %s\n", base);
+	//printf("LDAP Servers: %s\n", ldapservers);
 
-	if (ipa_check_hbac(thishost, username))
+	if (ipa_check_hbac(ldapservers, sysaccount, bindpw, thishost, username))
 		return PAM_SUCCESS;
 	else
 		return PAM_PERM_DENIED;
